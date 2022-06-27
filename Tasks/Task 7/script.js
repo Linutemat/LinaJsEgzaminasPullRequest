55f9695da52a024509b931465f27b041f3e80db5
Your Name~ <you@example.com>
2022-06-27 21:59:22 +0300

    7Uzd

---
+++ Tasks/Task 7/script.js
@@ -0,0 +1,22 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+'use strict';
+
+const audi = {
+  make: 'audi',
+  model: 'A6',
+  year: 2005,
+  color: 'white',
+};
+
+
+function showObjectKeys(carBrand) {
+  const objectKeys = Object.keys(carBrand);
+  return objectKeys
+};
+
+console.log(showObjectKeys(audi));
